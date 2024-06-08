083dfab92ec5b172ba27a8804acd2ebc15bf5055
riffat-firdous <160425768+riffat-firdous@users.noreply.github.com>
2024-06-08 02:23:16 -0700

    calculator

---
+++ calculator.js
@@ -0,0 +1,31 @@
+#! /user/bin/node
+import inquirer from "inquirer";
+//printing a Welcome Message
+console.log("\n \tWellcom To \`CodeWithRiffat\` -CLI Simple Calculator\n");
+//Asking Question from User through Inquirer
+let answer = await inquirer.prompt([
+    { message: " Enter First Number", type: "number", name: "firstNumber" },
+    { message: " Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// Condtional Statements if-Else
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
